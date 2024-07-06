dd266db4a533654015aecf71e7fed19c49fcf251
anatoliiprk <anatoliiprk@gmail.com>
2024-07-06 15:01:42 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_of_one_task = hours_spent / total_tasks
+print('Курс:', course_name, ', всего задач:', total_tasks, ', затрачено часов:', hours_spent, ', среднее время выполнения', time_of_one_task, 'часа.')
